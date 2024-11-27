dd6cba6d331161d0f6088ee35aa9f9947816e272
Jakub Pniewski <jp459481@students.mimuw.edu.pl>
2024-11-27 19:43:11 +0100

    basic programme structure

---
+++ poly.h
@@ -0,0 +1,60 @@
+#ifndef POLY_H
+#define POLY_H
+
+#include <cstddef>
+
+template <typename T, std::size_t N = 0> 
+class poly
+{
+public:
+
+    // KONSTRUKTORY
+
+    // Konstruktor bezargumentowy tworzy wielomian tożsamościowo równy zeru
+    poly(); // TODO: implement 
+
+    // Konstruktor kopiujący bądź przenoszący (jednoargumentowe), których argument jest odpowiednio typu const poly<U, M>& bądź poly<U, M>&&, gdzie M <= N, a typ U jest konwertowalny do typu T.
+    poly(const poly<U, M>&); // TODO: implement 
+    poly(const poly<U, M>&&); // TODO: implement 
+    !
+    // Konstruktor konwertujący (jednoargumentowy) o argumencie typu konwertowalnego do typu T tworzy wielomian rozmiaru 1.
+    // TODO: declare and implement 
+
+    // Konstruktor wieloargumentowy (dwa lub więcej argumentów) tworzy wielomian o współczynnikach takich jak wartości kolejnych argumentów. 
+    // Liczba argumentów powinna być nie większa niż rozmiar wielomianu N, a typ każdego argumentu powinien być r-referencją do typu konwertowalnego do typu T. 
+    // Wymagamy użycia „perfect forwarding”, patrz std::forward.
+    // TODO: declare and implement 
+
+    // ^^^^^^^^^^^^
+    // Należy zapoznać się z szablonem std::is_convertible i konceptem std::convertible_to
+
+
+    // Powyższe konstruktory nie umożliwiają stworzenia wielomianu rozmiaru jeden (czyli stałego), którego jedyny współczynnik jest wielomianem
+    // Dlatego należy zaimplementować funkcję const_poly, której argumentem jest wielomian p (obiekt typu poly) i która zwraca wielomian rozmiaru jeden, którego jedyny współczynnik to p.
+    // TODO: declare and implement
+
+    // OPERATORY PRZYPISANIA
+    // TODO: declare and implement
+
+    // OPERATORY ARYTMETYCZNE
+    // TODO: declare and implement
+
+    // OPERATOR INDEKSUJĄCY
+    // TODO: declare and implement
+
+    // METODA AT
+    // TODO: declare and implement
+
+    // METODA SIZE
+    // TODO: declare and implement
+
+private:
+    vector<T> a;
+};
+
+
+// FUNKCJA CROSS
+// TODO: declare and implement
+
+
+#endif // POLY_H
---
+++ poly_test_1.cpp
@@ -0,0 +1,91 @@
+#include "poly.h"
+#include <cstddef>
+#include <array>
+#include <type_traits>
+
+namespace
+{
+  template <typename T, std::size_t N>
+  constexpr bool operator==(const poly<T, N>& a, const poly<T, N>& b)
+  {
+    for (std::size_t i = 0; i < N; ++i)
+      if (!(a[i] == b[i]))
+        return false;
+    return true;
+  }
+
+  void test_static()
+  {
+    constexpr auto p = poly(2, 1);
+    static_assert(std::is_same_v<decltype(p), const poly<int, 2>>);
+    static_assert(p[0] == 2);
+    static_assert(p.at(1) == 3);
+    static_assert(p.at(0.5) == 2.5);
+    static_assert(p.at(p) == poly(4, 1));
+
+    static_assert(std::is_same_v<std::common_type_t<double, poly<int, 2>>, poly<double, 2>>);
+    static_assert(std::is_same_v<std::common_type_t<poly<int, 2>, poly<double, 2>>, poly<double, 2>>);
+
+    constexpr auto q = poly(poly(1.0, 2.0), 3.0, 4.0);
+    static_assert(std::is_same_v<decltype(q), const poly<poly<double, 2>, 3>>);
+    static_assert(q[0][0] == 1.0);
+    static_assert(q[0][1] == 2.0);
+    static_assert(q[1][0] == 3.0);
+    static_assert(q[1][1] == 0.0);
+    static_assert(q[2][0] == 4.0);
+    static_assert(q[2][1] == 0.0);
+    static_assert(q.at() == q);
+    static_assert(q.at(2.0, 3.0) == 29.0);
+    static_assert(q.at(2.0, 3.0, 4.0) == 29.0);
+    static_assert(q.at(2.0) == poly(23.0, 2.0));
+    static_assert(q.at(2.0).at(3.0) == q.at(2.0, 3.0));
+    static_assert(q.at(p) == poly(23.0, 21.0, 4.0, 0.0));
+    static_assert(q.at(-1, p) == poly(6.0, 2.0));
+    static_assert(q.at(std::array{2.0, 3.0}) == 29.0);
+
+    static_assert(std::is_same_v<decltype(p + q), poly<poly<double, 2>, 3>>);
+    static_assert(p + q == poly(poly(3.0, 2.0), 4.0, 4.0));
+    static_assert(-q == poly(poly(-1.0, -2.0), -3.0, -4.0));
+    static_assert(p - q == poly(poly(1.0, -2.0), -2.0, -4.0));
+    static_assert(q - p == poly(poly(-1.0, 2.0), 2.0, 4.0));
+    static_assert(std::is_same_v<decltype(p * q), poly<poly<double, 2>, 4>>);
+    static_assert(p * q == poly(poly(2.0, 4.0), poly(7.0, 2.0), 11.0, 4.0));
+    static_assert(cross(p, q) == poly(2.0 * q, q));
+    static_assert(cross(q, p) == poly(poly(p, 2.0 * p), const_poly(3.0 * p), const_poly(4.0 * p)));
+
+    constexpr auto pp(p);
+    static_assert(p == pp);
+    constexpr auto qq(q);
+    static_assert(q == qq);
+  }
+
+  template <typename T>
+  class weak_wrapper
+  {
+  private:
+    T a;
+  public:
+    weak_wrapper() {}
+    weak_wrapper(T a) : a(a) {}
+    weak_wrapper& operator-=(weak_wrapper b) {a -= b.a; return *this;}
+    weak_wrapper& operator+=(weak_wrapper b) {a += b.a; return *this;}
+    weak_wrapper& operator*=(weak_wrapper b) {a *= b.a; return *this;}
+    template <typename U>
+    friend weak_wrapper<U> operator*(weak_wrapper<U> a, weak_wrapper<U> b) {return a.a * b.a;}
+  };
+
+  void test_weak()
+  {
+    using t = weak_wrapper<int>;
+    poly(t(2)) * t(3);
+    poly(t(2)) + poly(t(3)); // Może się kompilować, ale nie musi.
+    // t(2) + t(3); // Skutkuje błędem podczas kompilowania.
+    // t(2) - t(3); // Skutkuje błędem podczas kompilowania.
+  }
+} // anonimowa przestrzeń nazw
+
+int main()
+{
+  test_static();
+  test_weak();
+}
